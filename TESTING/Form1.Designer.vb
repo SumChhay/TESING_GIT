3ae37282474f1a6f0a0da371cf4b3098dd632cbb
SumChhay <102141824+SumChhay@users.noreply.github.com>
2024-01-24 11:40:50 +0700

    Add files via upload

---
+++ TESTING/Form1.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.cbProtect = New System.Windows.Forms.CheckBox()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.SuspendLayout()
+        '
+        'cbProtect
+        '
+        Me.cbProtect.AutoSize = True
+        Me.cbProtect.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbProtect.Location = New System.Drawing.Point(12, 12)
+        Me.cbProtect.Name = "cbProtect"
+        Me.cbProtect.Size = New System.Drawing.Size(224, 36)
+        Me.cbProtect.TabIndex = 0
+        Me.cbProtect.Text = "Protect Screen"
+        Me.cbProtect.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStatus.ForeColor = System.Drawing.Color.Red
+        Me.lblStatus.Location = New System.Drawing.Point(242, 14)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(167, 32)
+        Me.lblStatus.TabIndex = 1
+        Me.lblStatus.Text = "Status: OFF"
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "Screen Protector"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(416, 62)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.cbProtect)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Screen Protector"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cbProtect As CheckBox
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+End Class
---
+++ TESTING/Form1.vb
@@ -0,0 +1,85 @@
+﻿Imports System.Runtime.InteropServices
+Imports Microsoft.Win32
+Public Class Form1
+    Dim onoff As Boolean
+    Declare Auto Function SetWindowDisplayAffinity Lib "User32.dll" Alias "SetWindowDisplayAffinity" (ByVal hWnd As Integer, ByVal dwAffinity As Integer) As Boolean
+
+
+    Private Const GWL_EXSTYLE As Integer = -20
+    Private Const WS_EX_LAYERED As Integer = &H80000
+    Private Const WS_EX_TRANSPARENT As Integer = &H20
+    Private Const WS_EX_TOOLWINDOW As Integer = &H80
+
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function SetWindowLong(ByVal hWnd As IntPtr, ByVal nIndex As Integer, ByVal dwNewLong As Integer) As Integer
+    End Function
+
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function GetWindowLong(ByVal hWnd As IntPtr, ByVal nIndex As Integer) As Integer
+    End Function
+
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function SetLayeredWindowAttributes(ByVal hWnd As IntPtr, ByVal crKey As Integer, ByVal bAlpha As Byte, ByVal dwFlags As Integer) As Boolean
+    End Function
+
+
+    Private Sub cbProtect_CheckedChanged(sender As Object, e As EventArgs) Handles cbProtect.CheckedChanged
+        If cbProtect.Checked = True Then
+            Dim screen As Screen
+            For Each screen In Screen.AllScreens
+                Dim form As New Form()
+                form.FormBorderStyle = FormBorderStyle.None
+                form.WindowState = FormWindowState.Maximized
+                form.StartPosition = FormStartPosition.Manual
+                form.ShowInTaskbar = False
+                form.ShowIcon = False
+                form.TopMost = True
+                form.BackColor = Color.Wheat
+                form.TransparencyKey = Color.Wheat
+                form.Location = screen.Bounds.Location
+                form.Size = screen.Bounds.Size
+                form.Show()
+                Dim hWnd As Integer = form.Handle
+
+                ' Then call the function as follows
+                SetWindowDisplayAffinity(hWnd, 1)
+                Dim styles As Integer = GetWindowLong(form.Handle, GWL_EXSTYLE)
+
+                SetWindowLong(form.Handle, GWL_EXSTYLE, styles Or WS_EX_LAYERED Or WS_EX_TRANSPARENT Or WS_EX_TOOLWINDOW)
+            Next
+            lblStatus.Text = "Status: ON"
+            lblStatus.ForeColor = Color.Green
+        Else
+            ' Close all forms after the loop
+            Dim formsToClose As New List(Of Form)()
+
+            For Each openForm As Form In Application.OpenForms
+                If openForm IsNot Me Then ' Exclude the current form
+                    formsToClose.Add(openForm)
+                End If
+            Next
+
+            For Each formToClose As Form In formsToClose
+                formToClose.Close()
+            Next
+            lblStatus.Text = "Status: OFF"
+            lblStatus.ForeColor = Color.Red
+        End If
+    End Sub
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            Me.Hide()
+            NotifyIcon1.Visible = True
+        End If
+    End Sub
+    Private Sub NotifyIcon1_MouseClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseClick
+        If e.Button = MouseButtons.Left Then
+            Me.Show()
+            Me.WindowState = FormWindowState.Normal
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        NotifyIcon1.Icon = Me.Icon
+    End Sub
+End Class
